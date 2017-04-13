0ec6c2be8e88e49c8a2b61a0ad2ea5315f197d77
Siddharth Vaknalli <ssvaknal@uwaterloo.ca>
2017-04-12 22:00:33 -0400

    Add description with typewriter effect and improve styling

---
+++ js/intro.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".role").typed({
+    strings: ["full stack developer.", "web designer."],
+    contentType: 'text',
+    backDelay: 1000,
+    callback: typingCallback
+  });
+});
+
+function typingCallback() {
+  setTimeout(function() {
+    $(".description").hide().text("Full Stack Developer | Web Designer").fadeIn("slow");
+  }, 1000)
+}
