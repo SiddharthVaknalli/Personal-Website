09b7b53d9027097f11e1f0d65abd18d50273c886
Siddharth Vaknalli <ssvaknal@uwaterloo.ca>
2017-04-16 15:34:31 -0400

    Submit contact form data through XHR request and sanitize form data on the backend

--- email.php
+++ email.php
@@ -1,41 +1,54 @@
 <?php
-  if (isset($_POST["submit"])) {
-    $error = false;
+  $error = "";
 
-    // Check if name has been entered
-    if (!$_POST['name']) {
-      echo "Please enter your name<br>";
-      $error = true;
+  // Check if name has been entered and sanitize it if true
+  if (!$_POST['name']) {
+    $error .= "Please enter your name.<br>";
+  }
+  else {
+    $_POST['name'] = strip_tags($_POST['name']);
+  }
+
+  // Check if email has been entered and sanitize and validate it if true
+  if(!$_POST['email']) {
+    $error .= "Please enter an email address.<br>";
+  } 
+  else {
+    $_POST['email'] = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+
+    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+      $error .= "Your email address is invalid.<br>";
     }
+  }
 
-    // Check if email has been entered and is valid
-    if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-      echo "Please enter a valid email address<br>";
-      $error = true;
+  //Check if message has been entered and sanitize it if true
+  if (!$_POST['message']) {
+    $error .= "Please enter a message.";
+  }
+  else {
+    $_POST['message'] = strip_tags($_POST['message']);
+  }
+
+  // If there are no errors, send the email
+  if (empty($error)) {
+    $name = $_POST['name'];
+    $from = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = 'sidd.v17@gmail.com'; 
+    $subject = 'New message from '.$name;
+
+    $email = mail($to, $subject, $message, $from);
+    if ($email) {
+      http_response_code(200);
+      echo "Email sent successfully!";
+    } else {
+      http_response_code(500);
+      echo "Sorry there was an error sending your message. Please try again later.";
     }
-
-    //Check if message has been entered
-    if (!$_POST['message']) {
-      echo "Please enter your message<br>";
-      $error = true;
-    }
-
-    // If there are no errors, send the email
-    if (!$error) {
-      $name = $_POST['name'];
-      $from = $_POST['email'];
-      $message = $_POST['message'];
-
-      $to = 'sidd.v17@gmail.com'; 
-      $subject = 'New message from your website!';
-      $body = "From: $name".$message;
-
-      $email = mail($to, $subject, $message, $from);
-      if ($email) {
-        echo "Email sent successfully!";
-      } else {
-        echo "Sorry there was an error sending your message. Please try again later.";
-      }
-    }
-	}
+  }
+  else {
+    http_response_code(400);
+    echo $error;
+  }
 ?>
--- js/main.js
+++ js/main.js
@@ -10,4 +10,44 @@ $(document).ready(function(){
       window.location.hash = hash;
     });
   });
+
+  var messageTimer = null;
+
+  // Submit contact form through an AJAX post request
+  $('#contact-form').submit(function(event) {
+    event.preventDefault();
+
+    var form = $(this);
+    var contactData = form.serialize();
+
+    var submitMessage = $('.submit-message');
+
+    $.ajax({
+      type: 'POST',
+      url: form.attr('action'),
+      data: contactData
+    })
+    .done(function(response) {
+      submitMessage.show();
+      submitMessage.html(response);
+      submitMessage.removeClass('red');
+      submitMessage.addClass('green');
+      clearTimeout(messageTimer);
+      messageTimer = setTimeout(function() {
+        submitMessage.fadeOut("slow");
+      }, 3000);
+      form[0].reset();
+    })
+    .fail(function(data) {
+      submitMessage.show();
+      submitMessage.html(data.responseText);
+      submitMessage.removeClass('green');
+      submitMessage.addClass('red');
+      clearTimeout(messageTimer);
+      messageTimer = setTimeout(function() {
+        submitMessage.fadeOut("slow");
+      }, 3000);
+    });
+  });
+
 });
